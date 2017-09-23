523214b37b1c70923f08b7fe3b27defdd54d8b81
lazy <1772885836@qq.com>
2017-09-23 21:27:32 +0800

    [*] add RxPermissions

--- app/src/main/java/com/example/lazy/kotlin/douban/splash/SplashActivity.kt
+++ app/src/main/java/com/example/lazy/kotlin/douban/splash/SplashActivity.kt
@@ -1,14 +1,19 @@
 package com.example.lazy.kotlin.douban.splash
 
+import android.Manifest
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.lazy.kotlin.R
 import com.example.lazy.kotlin.douban.main.MainActivity
+import com.tbruyelle.rxpermissions2.RxPermissions
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 
 class SplashActivity : AppCompatActivity() {
+    val PERMISSION_LIST = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
     companion object {
         var isSplashed = false
     }
@@ -25,12 +30,22 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun inHome() {
-        doAsync {
-            Thread.sleep(500)
-            uiThread {
-                startMainActivity()
-            }
-        }
+        RxPermissions(this)
+                .request(*PERMISSION_LIST)
+                .subscribe({
+                    if (it) {
+                        doAsync {
+                            Thread.sleep(500)
+                            uiThread {
+                                startMainActivity()
+                            }
+                        }
+                    } else {
+                        finish()
+                    }
+                }, {
+                    toast(it.message!!)
+                })
     }
 
     private fun startMainActivity() {
